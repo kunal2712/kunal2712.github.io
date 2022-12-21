32e8a144f5e7ad8c67b7c5ebb36bdd5ef6950ea2
kunal sharma <kunals7888@gmail.com>
2022-12-21 14:25:47 +0530

    Weather
    Forecast around the world

---
+++ script.js
@@ -0,0 +1,36 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9e54e692aamshf0397534f0229a5p1e5a65jsn96eac41826d4',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const getWeather = (city) => {
+
+	cityName.innerHTML = city
+
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city, options)
+		.then(response => response.json())
+		.then((response) => {
+
+			console.log(response)
+			cloud_pct.innerHTML = response.cloud_pct
+			temp.innerHTML = response.temp
+			feels_like.innerHTML = response.feels_like
+			humidity.innerHTML = response.humidity
+			min_temp.innerHTML = response.min_temp
+			max_temp.innerHTML = response.max_temp
+			wind_speed.innerHTML = response.wind_speed
+			wind_degrees.innerHTML = response.wind_degrees
+			sunrise.innerHTML = response.sunrise
+			sunset.innerHTML = response.sunset
+
+		})
+		.catch(err => console.error(err));
+}
+submit.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+
+getWeather("Delhi")
